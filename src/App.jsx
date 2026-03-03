05c992137ddbebe589dd9ab49dfe36bb66b881e9
Pawan Rajput <paw7672@gmail.com>
2026-03-03 11:01:56 +0530

    resolved the confilits

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
-import AppRoutes from "./AppRoutes";
+import AppRoutes from "./appRoutes";
 function App() {
   return (
     <>
